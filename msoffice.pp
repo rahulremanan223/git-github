71b9178a757bb81c5e7671f6434d43f5efc78c6d
RAHUL REMANAN <RAHUL REMANAN>
2017-07-04 10:22:07 +0530

    commitiing second time with new file and modifiction

--- msoffice.pp
+++ msoffice.pp
@@ -1,3 +1,6 @@
 package { 'Microsoft Office Enterprise 2007' : 
 	ensure => 'absent',
+
+djsjdhsjdh
+jhdjhjd
 }
---
+++ rahul.pp
@@ -0,0 +1,5 @@
+package { 'Jenkins 2.46.1': 
+	ensure => 'installed',
+	install_options => [ '/i', '/quiet', '/norestart' ],
+	source => "C:\temp\jenkins-2.46.1\jenkins.msi",
+}
